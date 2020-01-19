98ee86864229dbee136342a70af1abc44b84e0cb
Shez32 <mshahzeb737@gmail.com>
2020-01-19 21:56:04 +0500

    Completed Base Game

--- Bomberman/Assets/Scripts/Player.cs
+++ Bomberman/Assets/Scripts/Player.cs
@@ -1,40 +1,10 @@
-﻿/*
- * Copyright (c) 2017 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
- * or sale is expressly withheld.
- *    
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 using System;
 
 public class Player : MonoBehaviour
 {
-
+    public GlobalStateManager globalStateManager;
     //Player parameters
     [Range (1, 2)] //Enables a nifty slider in the editor
     public int playerNumber = 1;
@@ -43,6 +13,7 @@ public class Player : MonoBehaviour
     public bool canDropBombs = true;
     //Can the player drop bombs?
     public bool canMove = true;
+    public bool isDead = false;
     //Can the player move?
 
     private int bombs = 2;
@@ -176,8 +147,9 @@ public class Player : MonoBehaviour
     private void DropBomb ()
     {
         if (bombPrefab)
-        { //Check if bomb prefab is assigned first
-
+        { 
+            Instantiate(bombPrefab, new Vector3(Mathf.RoundToInt(myTransform.position.x), myTransform.position.y,
+             Mathf.RoundToInt(myTransform.position.z)), bombPrefab.transform.rotation);
         }
     }
 
@@ -186,6 +158,9 @@ public class Player : MonoBehaviour
         if (other.CompareTag ("Explosion"))
         {
             Debug.Log ("P" + playerNumber + " hit by explosion!");
+            isDead = true;
+            globalStateManager.PlayerDied(playerNumber);
+            Destroy(gameObject);
         }
     }
 }
